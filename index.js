7714b98531d1a48ec5dcea5b69c9c8a42e085204
badakim2187 <badakim2187@gmail.com>
2021-05-15 23:40:49 +0900

    modifying DOM with JS

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
-function sayHello(name, age){
-    console.log('Hello!', name, "you have", age, "years of age.");
-}
-sayHello("Nicolas", 15);
-
-function sayHello2(name, age){
-    console.log(`Hello ${name} you are ${age} yaers`);
-}
-
-sayHello2("Nicolas", 15);
-
-function sayHello3(name, age){
-    return `Hello ${name} you are ${age} years`;
-}
-const greetNicolas = sayHello3("Nicalas", 15)
-
-console.log(greetNicolas)
-
-const calculator = {
-    plus: function(a,b){
-        return a + b;
-    }
-}
-
-//console.log(greetNicolas2)
-const plus = calculator.plus(5, 5)
-console.log(plus)
-
-
-
+//const id = document.getElementById("title");
+const title = document.querySelector("#title");
+title.innerHTML = "Hi! From JS";
+title.style.color = "red";
+document.title = 'I own you now';

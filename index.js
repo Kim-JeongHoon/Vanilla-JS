712a0d9ee2c7404bbfee620211412daef7e87155
badakim2187 <badakim2187@gmail.com>
2021-05-15 23:01:35 +0900

    test3

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-const KimInfo = {
-    name : "Kim Jeong Hoon",
-    age : 31,
-    gender : "Male",
-    isHandsome : true,
-    favorites : ["Along the gods", "LOTR", "Oldboy"],
-    favfood : [
-        {
-            name: "Hamburger",
-            fatty : false
-        },
-        {
-            name: "Kimchi",
-            fatty : true
+function sayHello(name, age){
+    console.log('Hello!', name, "you have", age, "years of age.");
+}
+sayHello("Nicolas", 15);
 
-        }
-    ]
+function sayHello2(name, age){
+    console.log(`Hello ${name} you are ${age} yaers`);
 }
 
-console.log(KimInfo)
+sayHello2("Nicolas", 15);
+
+function sayHello3(name, age){
+    return `Hello ${name} you are ${age} years`;
+}
+const greetNicolas = sayHello3("Nicalas", 15)
+
+console.log(greetNicolas)
+
+const calculator = {
+    plus: function(a,b){
+        return a + b;
+    }
+}
+
+//console.log(greetNicolas2)
+const plus = calculator.plus(5, 5)
+console.log(plus)
+
+
+
